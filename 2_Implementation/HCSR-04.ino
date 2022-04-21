4f11c532a491b89b9c6a85116cd7e588ed9720de
ganesh-pallapothu2000 <92217788+ganesh-pallapothu2000@users.noreply.github.com>
2022-04-21 17:25:27 +0530

    Add files via upload

---
+++ 2_Implementation/HCSR-04.ino
@@ -0,0 +1,56 @@
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>
+
+static volatile int pulse = 0;
+static volatile int i = 0;
+
+int main(void)
+{
+  int16_t count_a = 0; 
+  char show_a[16];
+  DDRD = 0b11111011;
+  _delay_ms(50);
+
+  GICR |= 1<<INT0;
+  MCUCR |= 1<<ISC00;
+
+  sei();
+ 
+  while(1)
+  { 
+    PORTD |= 1<<PIND0;
+    _delay_us(15);
+
+    PORTD &= ~(1<<PIND0);
+    count_a = pulse/58;
+
+    Send_A_String("Distance Sensor");
+    GoToMrLCDLocation(1,2);
+    Send_A_String("Distance=");
+    itoa(count_a,show_a,10);
+    Send_A_String(show_a);
+    Send_A_String(" ");
+    GoToMrLCDLocation(13,2);
+    Send_A_String("cm");
+    GoToMrLCDLocation(1,1);
+  }
+}
+
+ISR(INT0_vect)
+{
+  if(i == 0)
+  {
+    TCCR1B |= 1<<CS10;
+    i = 1;
+  }
+  else
+  {
+    TCCR1B = 0;
+    pulse = TCNT1;
+    TCNT1 = 0;
+    i = 0;
+  }
+}
